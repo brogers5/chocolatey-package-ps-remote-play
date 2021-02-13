f822739270f2fdc4a3a673390b5908ea2196f402
Danny J Kendall <d.manicx100@gmail.com>
2021-02-13 09:50:49 +0000

    edits to moderating packages
    # Conflicts:
    #	anytoiso/anytoiso.3.9.6.nupkg
    #	anytoiso/anytoiso.nuspec
    #	imazing/imazing.2.13.2.nupkg
    #	imazing/imazing.nuspec
    #	imazing/tools/chocolateyinstall.ps1
    #	yogaDNS/YogaDNS.1.1.6.nupkg
    #	yogaDNS/yogaDNS.nuspec

--- lagrange/tools/chocolateyinstall.ps1
+++ lagrange/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $packageName = 'Lagrange'
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/skyjake/lagrange/releases/download/v1.1.1/lagrange_v1.1.1_windows-x64_setup.exe'
-$checksum = 'DF056DAF7AB318D5D8EF676F42BA303138D64981BF396A62244F4AA848E7095A'
+$url = 'https://github.com/skyjake/lagrange/releases/download/v1.1.2/lagrange_v1.1.2_windows-x64_setup.exe'
+$checksum = '15AB45C77B93D15C5461FE6757648B2F7AA1B24759764F57F11CEDCB18F861F5'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
