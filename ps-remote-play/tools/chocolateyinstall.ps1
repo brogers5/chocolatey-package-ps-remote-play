eff1f80cb24ff07c6a66d44b84e704aba5a7776f
Dan Kendall <d.manicx100@gmail.com>
2022-03-28 22:45:49 +0100

    First update with brogers5 AU script

--- ps-remote-play/tools/chocolateyinstall.ps1
+++ ps-remote-play/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
-$packageName = 'ps-remote-play'
+﻿$packageName = 'ps-remote-play'
 $installerType = 'exe'
 $silentArgs = '/S /v/qn'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://remoteplay.dl.playstation.net/remoteplay/module/win/RemotePlayInstaller.exe'
-$checksum = '8F71ABDA6E2F227CB0EDB79ED4FD5617EC727BE8D81C1A7919241292AC946CF9'
+$checksum = '7ca976a86c7a2f320a543beb09e73a221ef0c849b8b0de8295e2fb0f63722dcf'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
