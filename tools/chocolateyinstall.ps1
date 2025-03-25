61bed726b43e600ff5e6b010506dacf5aabdc527
brogers5 <6869577+brogers5@users.noreply.github.com>
2025-03-25 15:48:03 -0400

    Update to 8.0.0.14120

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -5,9 +5,9 @@ Confirm-Win10 -ReqBuild 14393
 $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'MSI'
-    url            = 'https://remoteplay.dl.playstation.net/remoteplay/module/win/RemotePlayInstaller_7.5.0.08061_Win32.msi'
+    url            = 'https://remoteplay.dl.playstation.net/remoteplay/module/win/RemotePlayInstaller_8.0.0.14120_Win32.msi'
     softwareName   = 'PS Remote Play'
-    checksum       = '6147c767e6c894150d08b350cb0c0f7ab4d8fe9ad1750d1f3d99dac387eca89b'
+    checksum       = '1ddba7e05856b20c60dae35fb4ef41b05d733863cea959901e21b7b46dff33e5'
     checksumType   = 'sha256'
     validExitCodes = @(0, 3010, 1641)
     silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
